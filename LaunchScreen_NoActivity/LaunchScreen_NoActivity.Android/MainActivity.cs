90a66be089a1364dbd18c4dfee0b9582d5c62ec1
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2018-02-20 21:37:04 +0900

    Add project files.

---
+++ LaunchScreen_NoActivity/LaunchScreen_NoActivity.Android/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace LaunchScreen_NoActivity.Droid
+{
+    [Activity(Label = "LaunchScreen_NoActivity", Icon = "@drawable/icon", Theme = "@style/LaunchScreenTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            SetTheme(Resource.Style.MainTheme);
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
